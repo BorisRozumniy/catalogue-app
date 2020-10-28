03977381c59c1afe2f1c78d656936a67acaeba0b
Boris <rozumniy.boris.work@gmail.com>
2020-10-29 00:38:06 +0200

    feat: styled Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
 import { frontendUrls } from "./routes/frontendUrls";
 
@@ -8,32 +9,29 @@ import Catalogue from "./modules/catalogue/Catalogue";
 import Registration from "./modules/registration/Registration";
 import EditProduct from "./modules/product/EditProduct";
 import AddProduct from "./modules/product/AddProduct";
-import { Container } from "reactstrap";
 
 const App = () => {
   return (
-    <Container>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route path={frontendUrls.catalogue}>
-            <Catalogue />
-          </Route>
-          <Route path={frontendUrls.registration}>
-            <Registration />
-          </Route>
-          <Route path={frontendUrls.editProduct}>
-            <EditProduct />
-          </Route>
-          <Route path={frontendUrls.addProduct}>
-            <AddProduct />
-          </Route>
-          <Route path={frontendUrls.home}>
-            <h1>Welcome to our store</h1>
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path={frontendUrls.catalogue}>
+          <Catalogue />
+        </Route>
+        <Route path={frontendUrls.registration}>
+          <Registration />
+        </Route>
+        <Route path={frontendUrls.editProduct}>
+          <EditProduct />
+        </Route>
+        <Route path={frontendUrls.addProduct}>
+          <AddProduct />
+        </Route>
+        <Route exact path={frontendUrls.home}>
+          <h1>Welcome to our store</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/modules/header/Header.js
+++ src/modules/header/Header.js
@@ -1,27 +1,60 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Navbar, Nav, NavItem } from "reactstrap";
+import { Navbar, Nav, NavItem, Container } from "reactstrap";
 import { frontendUrls } from "../../routes/frontendUrls";
 
 const Header = () => (
-    <Navbar>
-        <Nav>
-            <NavItem>
-                <NavLink to={frontendUrls.home}>home</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink to={frontendUrls.catalogue}>catalogue</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink to={frontendUrls.registration}>registration</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink to={frontendUrls.editProduct}>editProduct</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink to={frontendUrls.addProduct}>addProduct</NavLink>
-            </NavItem>
-        </Nav>
+    <Navbar color="light" light expand="md" className="mb-4">
+        <Container className=" justify-content-center ">
+            <Nav active className="text-uppercase p-3">
+                <NavItem active className="mr-2">
+                    <NavLink
+                        exact
+                        to={frontendUrls.home}
+                        activeClassName="bg-warning"
+                        className="p-3"
+                    >
+                        home
+                    </NavLink>
+                </NavItem>
+                <NavItem className="mr-2">
+                    <NavLink
+                        to={frontendUrls.catalogue}
+                        activeClassName="bg-warning"
+                        className="p-3"
+                    >
+                        catalogue
+                    </NavLink>
+                </NavItem>
+                <NavItem className="mr-2">
+                    <NavLink
+                        to={frontendUrls.registration}
+                        activeClassName="bg-warning"
+                        className="p-3"
+                    >
+                        registration
+                    </NavLink>
+                </NavItem>
+                <NavItem className="mr-2">
+                    <NavLink
+                        to={frontendUrls.editProduct}
+                        activeClassName="bg-warning"
+                        className="p-3"
+                    >
+                        editProduct
+                    </NavLink>
+                </NavItem>
+                <NavItem className="mr-2">
+                    <NavLink
+                        to={frontendUrls.addProduct}
+                        activeClassName="bg-warning"
+                        className="p-3"
+                    >
+                        addProduct
+                    </NavLink>
+                </NavItem>
+            </Nav>
+        </Container>
     </Navbar>
 )
 
