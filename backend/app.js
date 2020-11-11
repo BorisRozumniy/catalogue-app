9a008cc2161434a00825d9a99e4ee06cff968c61
Boris <rozumniy.boris.work@gmail.com>
2020-11-11 06:36:19 +0200

    mysql

--- backend/app.js
+++ backend/app.js
@@ -127,3 +127,20 @@ const server = http.createServer((req, res) => {
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
+
+const mysql = require("mysql2");
+  
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "usersdb",
+  password: "пароль_от_сервера"
+});
+connection.connect(function(err){
+  if (err) {
+    return console.error("Ошибка: " + err.message);
+  }
+  else{
+    console.log("Подключение к серверу MySQL успешно установлено");
+  }
+});
